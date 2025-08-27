3197efe6d783a05dc5265d71fbfebbcbff290ea2
Moumita Dey <talktomoumitadey@gmail.com>
2025-08-28 01:04:37 +0530

    Refactor initialization of largest and secondLargest using Integer.MIN_VALUE

--- src/edu/logic/solutions/FindSecondLargestElement.java
+++ src/edu/logic/solutions/FindSecondLargestElement.java
@@ -8,8 +8,10 @@ public class FindSecondLargestElement {
         System.out.println("Enter the size of the array : ");
         int size = scanner.nextInt();
         int arr[] = new int[size];//4
-        int largest= arr[0];//5
-        int secondLargest = arr[0];//5
+        //int largest= arr[0];//5
+        //int secondLargest = arr[0];//5
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
 
         for(int i = 0;i<arr.length;i++){
             System.out.println("Enter the element : "+(i+1));

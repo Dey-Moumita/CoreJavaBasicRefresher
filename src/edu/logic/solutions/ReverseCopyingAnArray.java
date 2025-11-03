8f77ba172ca471439adb78c0abbab93cebc4ccfb
Moumita Dey <talktomoumitadey@gmail.com>
2025-11-03 11:35:56 +0530

    Added some comment lines

--- src/edu/logic/solutions/ReverseCopyingAnArray.java
+++ src/edu/logic/solutions/ReverseCopyingAnArray.java
@@ -19,7 +19,11 @@ public class ReverseCopyingAnArray {
 //        }
 
         for(int i = 0;i<arr1.length;i++){
-            arr2[i] = arr1[(arr1.length-1)-i];
+            arr2[i] = arr1[(arr1.length-1)-i];//arr1[(5-1)-0]=arr[4] so arr[4] is entered in arr2[0]
+            //arr2[1]=arr1[(5-1)-1]->arr1[3]
+            //arr2[2]=arr1[(5-1)-2]->arr1[2]
+            //arr2[3]=arr1[(5-1)-3]->arr1[1]
+            //arr2[3]=arr1[(5-1)-4]->arr1[0]
         }
         System.out.println("Copied array is : ");
         for(int i = 0;i<arr2.length;i++){
